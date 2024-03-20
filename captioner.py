04b5fbf6b1013c559b72f29afff03d3c387afcf0
Pranjalya Tiwari <pranjalyawarrior@gmail.com>
2024-03-20 23:54:21 +0530

    :dog2: captioner patch

--- captioner.py
+++ captioner.py
@@ -5,6 +5,8 @@ class Captioner:
     def __init__(self, PROMPT="The main subject of this picture is a"):
         print("Initializing captioner...")
 
+        self.PROMPT = PROMPT
+
         self.captioner = pipeline(
             "image-to-text",
             model="Salesforce/blip-image-captioning-base",
@@ -14,5 +16,5 @@ class Captioner:
     def derive_caption(self, image, max_new_tokens=20):
         result = self.captioner(image, max_new_tokens)
         raw_caption = result[0]["generated_text"]
-        caption = raw_caption.lower().replace(PROMPT.lower(), "").strip()
+        caption = raw_caption.lower().replace(self.PROMPT.lower(), "").strip()
         return caption
